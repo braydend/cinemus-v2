8ce0e0de9964b8179929a8d6383f907fdf7350da
Brayden Di Luca <bray.diluca@estimateone.com>
2023-09-17 11:52:38 +1000

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,45 @@
 <script>
 	import '../app.postcss';
+	import * as Avatar from '$lib/components/ui/avatar';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	$: isAuthed = false;
+
+	const handleLogin = () => {
+		isAuthed = true;
+	};
+
+	const handleLogout = () => {
+		isAuthed = false;
+	};
 </script>
 
+<nav
+	class="w-full h-16 flex flex-row justify-between px-4 border-b-[0.5px] border-gray-600 bg-black"
+>
+	<div class="flex items-center justify-center text-white">LOGO</div>
+	<div class="flex items-center justify-center">
+		{#if isAuthed}
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger>
+					<Avatar.Root>
+						<!-- Dynamically set with user data from session -->
+						<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+						<Avatar.Fallback>CN</Avatar.Fallback>
+					</Avatar.Root>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content>
+					<DropdownMenu.Group>
+						<DropdownMenu.Label>Preferences</DropdownMenu.Label>
+						<DropdownMenu.Separator />
+						<DropdownMenu.Item on:click={handleLogout}>Logout</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		{:else}
+			<Button on:click={handleLogin}>Log in</Button>
+		{/if}
+	</div>
+</nav>
 <slot />
