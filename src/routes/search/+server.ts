eb973b1f33999e7cebb5c4affd107a25978747a5
Brayden <braydendiluca@gmail.com>
2023-09-19 19:45:56 +1000

    Add posters to media search endpoint

--- src/routes/search/+server.ts
+++ src/routes/search/+server.ts
@@ -1,27 +1,48 @@
 import { error, json } from '@sveltejs/kit';
 import { searchMovies, searchShows } from '../../lib/tmdb/media.js';
+import { getConfiguration } from '../../lib/tmdb/configuration.js';
 
 export async function GET({ url }) {
 	const mediaType = url.searchParams.get('type');
 	const query = url.searchParams.get('query') ?? '';
 
+	const tmdbConfigPromise = getConfiguration();
+
 	if (query === '') {
 		throw error(400, 'No search query');
 	}
 	if (mediaType === 'show') {
-		const results = await searchShows(query);
+		const [results, config] = await Promise.all([searchShows(query), tmdbConfigPromise]);
 
-		return json({ results });
+		const resultsWithImages = results.results.map((r) => ({
+			...r,
+			poster: `${config.images.secure_base_url}${config.images.poster_sizes[0]}${r.poster_path}`
+		}));
+
+		return json({ results: resultsWithImages });
 	}
 	if (mediaType === 'movie') {
-		const results = await searchMovies(query);
+		const [results, config] = await Promise.all([searchMovies(query), tmdbConfigPromise]);
 
-		return json({ results });
+		const resultsWithImages = results.results.map((r) => ({
+			...r,
+			poster: `${config.images.secure_base_url}${config.images.poster_sizes[0]}${r.poster_path}`
+		}));
+
+		return json({ results: resultsWithImages });
 	}
 
 	//TODO: replace with multi search endpoint
-	const movieResults = await searchMovies(query);
-	const showResults = await searchShows(query);
+	const [movieResults, showResults, config] = await Promise.all([
+		searchMovies(query),
+		searchShows(query),
+		tmdbConfigPromise
+	]);
 
-	return json({ results: [...movieResults.results, ...showResults.results] });
+	const resultsWithImages = [...movieResults.results, ...showResults.results].map((r) => ({
+		...r,
+		poster: `${config.images.secure_base_url}${config.images.poster_sizes[0]}${r.poster_path}`
+	}));
+
+	return json({ results: resultsWithImages });
 }
