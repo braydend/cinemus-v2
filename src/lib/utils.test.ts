ce8fb832663efc47ed03197aa281afc861b8a9f3
Brayden Di Luca <bray.diluca@estimateone.com>
2023-10-06 09:59:08 +1100

    Add tests for sentenceCase function

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect } from 'vitest';
-import { truncate } from './utils';
+import { sentenceCase, truncate } from './utils';
 
 describe('utils tests', () => {
-	describe('truncate', () => {
+	describe('truncate()', () => {
 		it('truncates to default length (10)', () => {
 			const input = 'long string gets cut off';
 			const expected = 'long st...';
@@ -31,6 +31,40 @@ describe('utils tests', () => {
 			const input = 'long string does not get cut off';
 			const expected = 'long string does not get cut off';
 			const result = truncate(input, 50);
+
+			expect(result).toBe(expected);
+		});
+	});
+
+	describe('senctenceCase()', () => {
+		it('correctly transforms lowercase string', () => {
+			const input = 'abcd';
+			const expected = 'Abcd';
+			const result = sentenceCase(input);
+
+			expect(result).toBe(expected);
+		});
+
+		it('correctly transforms uppercase string', () => {
+			const input = 'ABCD';
+			const expected = 'Abcd';
+			const result = sentenceCase(input);
+
+			expect(result).toBe(expected);
+		});
+
+		it('correctly transforms mixed-case string', () => {
+			const input = 'AbcD';
+			const expected = 'Abcd';
+			const result = sentenceCase(input);
+
+			expect(result).toBe(expected);
+		});
+
+		it('only transforms first character to uppercase', () => {
+			const input = 'AbcD EFgh';
+			const expected = 'Abcd efgh';
+			const result = sentenceCase(input);
 
 			expect(result).toBe(expected);
 		});
