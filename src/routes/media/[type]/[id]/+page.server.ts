476af792af8e9b950a43c3672f9848066e5712cb
Brayden Di Luca <bray.diluca@estimateone.com>
2023-10-10 20:55:03 +1100

    Add region selector component to media page

--- src/routes/media/[type]/[id]/+page.server.ts
+++ src/routes/media/[type]/[id]/+page.server.ts
@@ -8,7 +8,7 @@ import {
 import { getConfiguration } from '../../../../lib/tmdb';
 import { db } from '$lib/db';
 import type { CustomSession } from '$lib/types';
-import { listedMedia, media, watchlist } from '$lib/db/schema';
+import { listedMedia, media, userPreferences, watchlist } from '$lib/db/schema';
 import { and, eq } from 'drizzle-orm';
 
 /** @type {import('./$types').PageServerLoad} */
@@ -16,6 +16,13 @@ export async function load({ params, locals }) {
 	const { type, id } = params;
 	const userId = ((await locals.getSession())?.user as CustomSession)?.id;
 
+	const selectedRegionPromise = userId
+		? db
+				.select({ region: userPreferences.region })
+				.from(userPreferences)
+				.where(eq(userPreferences.userId, userId))
+				.limit(1)
+		: undefined;
 	const regionsPromise = getWatchProviderRegions();
 	const configPromise = getConfiguration();
 	const isListedPromise = db
@@ -33,19 +40,26 @@ export async function load({ params, locals }) {
 
 	if (type === 'show') {
 		const result = await getShow(id);
-		const [regions, showProviders, config, isListed] = await Promise.all([
-			regionsPromise,
+		const [selectedRegion, showProviders, config, isListed, regions] = await Promise.all([
+			selectedRegionPromise,
 			getShowWatchProviders(id),
 			configPromise,
-			isListedPromise
+			isListedPromise,
+			regionsPromise
 		]);
 
 		const {
 			images: { secure_base_url, logo_sizes, poster_sizes }
 		} = config;
 
+		const regionPreference = regions.results.find(
+			(region) => region.iso_3166_1 === selectedRegion?.[0]?.region
+		);
+
 		return {
-			regions,
+			region: regionPreference
+				? { value: regionPreference?.iso_3166_1, label: regionPreference?.english_name }
+				: undefined,
 			providers: showProviders,
 			media: result,
 			logoBase: `${secure_base_url}${logo_sizes.at(-1)}`,
@@ -57,19 +71,26 @@ export async function load({ params, locals }) {
 
 	if (type === 'movie') {
 		const result = await getMovie(id);
-		const [regions, movieProviders, config, isListed] = await Promise.all([
-			regionsPromise,
+		const [selectedRegion, movieProviders, config, isListed, regions] = await Promise.all([
+			selectedRegionPromise,
 			getMovieWatchProviders(id),
 			configPromise,
-			isListedPromise
+			isListedPromise,
+			regionsPromise
 		]);
+
+		const regionPreference = regions.results.find(
+			(region) => region.iso_3166_1 === selectedRegion?.[0]?.region
+		);
 
 		const {
 			images: { secure_base_url, logo_sizes, poster_sizes }
 		} = config;
 
 		return {
-			regions,
+			region: regionPreference
+				? { value: regionPreference?.iso_3166_1, label: regionPreference?.english_name }
+				: undefined,
 			providers: movieProviders,
 			media: result,
 			logoBase: `${secure_base_url}${logo_sizes.at(-1)}`,
