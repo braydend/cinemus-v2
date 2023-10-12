71d458f7a183edabcb374f04aead0e89f216d690
Brayden Di Luca <bray.diluca@estimateone.com>
2023-10-12 21:23:05 +1100

    Move media hydration to library function

--- src/routes/watchlist/+page.server.ts
+++ src/routes/watchlist/+page.server.ts
@@ -1,10 +1,9 @@
 import { eq } from 'drizzle-orm';
 import { db } from '../../lib/db/index';
 import { watchlist } from '../../lib/db/schema';
-import { getConfiguration } from '../../lib/tmdb/configuration';
-import { getMovie, getShow } from '../../lib/tmdb/media';
 import { error } from '@sveltejs/kit';
 import type { CustomSession } from '../../lib/types';
+import { hydrateList } from '$lib/hydrater';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals }) {
@@ -17,32 +16,8 @@ export async function load({ locals }) {
 		where: eq(watchlist.userId, userId),
 		with: { listedMedia: { with: { media: true } } }
 	});
-	const [tmdbConfig, ...tmdbMedia] = await Promise.all([
-		getConfiguration(),
-		...(list?.listedMedia.map(({ media: { type, tmdbId } }) => {
-			if (type === 'movie') {
-				return getMovie(tmdbId.toString(10));
-			}
-			return getShow(tmdbId.toString(10));
-		}) ?? [])
-	]);
 
-	const hydratedMedia =
-		list?.listedMedia.map(({ isWatched, rating, media: { tmdbId, type }, mediaId }) => {
-			const fetchedMedia = tmdbMedia.find(({ id }) => id === tmdbId);
-			return {
-				isWatched,
-				rating,
-				poster: `${tmdbConfig.images.secure_base_url}${tmdbConfig.images.poster_sizes.at(-1)}${
-					fetchedMedia?.poster_path
-				}`,
-				title: fetchedMedia?.__type === 'show' ? fetchedMedia.name : fetchedMedia?.title,
-				tmdbID: fetchedMedia?.id,
-				genres: fetchedMedia?.genres.map(({ name }) => name),
-				mediaId,
-				type
-			};
-		}) ?? [];
+	const hydratedList = await hydrateList(list?.listedMedia ?? []);
 
-	return { list: hydratedMedia };
+	return { list: hydratedList };
 }
