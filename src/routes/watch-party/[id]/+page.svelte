8ad247afc424ee06b8a0cf59a94e3831f9b539ea
Brayden Di Luca <bray.diluca@estimateone.com>
2023-10-12 21:23:24 +1100

    Render watch party

---
+++ src/routes/watch-party/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import WatchPartyMediaRow from '$lib/components/mediaRow/WatchPartyMediaRow.svelte';
+
+	export let data;
+</script>
+
+<!-- TODO: add join watch party endpoint -->
+<div>Watch party</div>
+{#each data.media as media}
+	<WatchPartyMediaRow media={{ ...media.media, users: media.users }} />
+{/each}
