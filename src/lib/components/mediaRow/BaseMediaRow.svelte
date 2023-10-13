c0afbe541983d6d202bcabee5976ac8f52528a8a
Brayden Di Luca <bray.diluca@estimateone.com>
2023-10-13 22:21:59 +1100

    Split MediaRow component into watchlist and watchParty specific components

---
+++ src/lib/components/mediaRow/BaseMediaRow.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Badge from '$lib/components/ui/badge/badge.svelte';
+	import { sentenceCase } from '$lib/utils';
+	import type { HydratedMedia } from '$lib/types';
+
+	export let media: HydratedMedia;
+</script>
+
+<div class="flex flex-row gap-4 items-center">
+	<img src={media.poster} alt={media.title} width={64} height={'auto'} />
+	<div class="flex flex-row justify-between w-full overflow-x-hidden">
+		<div>
+			<Badge class="text-white" variant="outline">{sentenceCase(media.type)}</Badge>
+			<h2
+				class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				{media.title}
+			</h2>
+			<div class="flex flex-row flex-wrap gap-2 overflow-x-hidden">
+				{#each media.genres as genre}
+					<Badge variant="secondary">{genre}</Badge>
+				{/each}
+			</div>
+		</div>
+		<slot name="actions" />
+	</div>
+</div>
---
+++ src/lib/components/mediaRow/Clapper.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	export let label: string;
+</script>
+
+<div class="container">
+	<div class="clapper">
+		<div class="top" />
+		<div class="bottom" />
+		<div class="base">{label}</div>
+	</div>
+</div>
+
+<style>
+	.clapper {
+		width: fit-content;
+	}
+
+	.top {
+		background: repeating-linear-gradient(45deg, #ffffff, #ffffff 10px, #000000 10px, #000000 20px);
+		width: 100px;
+		height: 20px;
+		animation: clap 0.7s;
+		border-radius: 4px 4px 0 0;
+	}
+
+	.bottom {
+		background: repeating-linear-gradient(
+			135deg,
+			#ffffff,
+			#ffffff 10px,
+			#000000 10px,
+			#000000 20px
+		);
+		width: 100px;
+		height: 20px;
+	}
+
+	.base {
+		background: black;
+		width: 100px;
+		height: 60px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 2px solid white;
+		border-radius: 0 0 4px 4px;
+	}
+
+	@keyframes clap {
+		0% {
+			rotate: 0deg;
+			transform-origin: bottom left;
+		}
+		50% {
+			rotate: -40deg;
+			transform-origin: bottom left;
+		}
+		100% {
+			rotate: 0deg;
+			transform-origin: bottom left;
+		}
+	}
+
+	@keyframes clapperText {
+		0% {
+			color: black;
+		}
+		99% {
+			color: black;
+		}
+		100% {
+			color: white;
+		}
+	}
+</style>
