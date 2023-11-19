a34b83a5956f6cbc4e741a1ef1109f59c28a70f1
Brayden Di Luca <bray.diluca@estimateone.com>
2023-11-19 18:11:30 +1100

    Add MSW mocks for region data

---
+++ tests/msw/mocks/regions.ts
@@ -0,0 +1,47 @@
+export const watchProviderRegions = [
+	{
+		iso_3166_1: 'AD',
+		english_name: 'Andorra',
+		native_name: 'Andorra'
+	},
+	{
+		iso_3166_1: 'AE',
+		english_name: 'United Arab Emirates',
+		native_name: 'United Arab Emirates'
+	},
+	{
+		iso_3166_1: 'AG',
+		english_name: 'Antigua and Barbuda',
+		native_name: 'Antigua & Barbuda'
+	},
+	{
+		iso_3166_1: 'AL',
+		english_name: 'Albania',
+		native_name: 'Albania'
+	},
+	{
+		iso_3166_1: 'AO',
+		english_name: 'Angola',
+		native_name: 'Angola'
+	},
+	{
+		iso_3166_1: 'AR',
+		english_name: 'Argentina',
+		native_name: 'Argentina'
+	},
+	{
+		iso_3166_1: 'AT',
+		english_name: 'Austria',
+		native_name: 'Austria'
+	},
+	{
+		iso_3166_1: 'AU',
+		english_name: 'Australia',
+		native_name: 'Australia'
+	},
+	{
+		iso_3166_1: 'AZ',
+		english_name: 'Azerbaijan',
+		native_name: 'Azerbaijan'
+	}
+];
