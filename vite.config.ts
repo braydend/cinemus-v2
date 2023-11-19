e0af8d28538bd9b82b1fac3f0910ca977280bfed
Brayden Di Luca <bray.diluca@estimateone.com>
2023-11-19 16:42:21 +1100

    Configure coverage stats and thresholds

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,15 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		globals: true,
 		environment: 'jsdom',
-		setupFiles: 'vitest-setup.ts'
+		setupFiles: 'vitest-setup.ts',
+		coverage: {
+			all: true,
+			include: ['src/**/*.ts', 'src/**/*.svelte'],
+			exclude: ['src/lib/components/ui'],
+			lines: 50,
+			branches: 70,
+			statements: 50,
+			functions: 50
+		}
 	}
 });
