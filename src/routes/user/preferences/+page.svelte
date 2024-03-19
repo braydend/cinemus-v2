7d25960a177ba070464dc70b750bf8803448eb65
Brayden Di Luca <bray.diluca@estimateone.com>
2024-03-20 09:46:52 +1100

    [WIP]

--- src/routes/user/preferences/+page.svelte
+++ src/routes/user/preferences/+page.svelte
@@ -1,48 +1,7 @@
 <script>
-	import RegionSelect from '$lib/components/regionSelect/regionSelect.svelte';
-	import Button from '$lib/components/ui/button/button.svelte';
-	import Input from '$lib/components/ui/input/input.svelte';
-	import Label from '$lib/components/ui/label/label.svelte';
-	import { errorTheme, successTheme } from '$lib/toaster';
-	import { toast } from '@zerodevx/svelte-toast';
+	import UserPreferences from './UserPreferences.svelte';
 
 	export let data;
-
-	let selectedRegion = data.region ?? { value: '', label: '' };
-	let username = data.userName ?? '';
-
-	const handleSave = async () => {
-		const resp = await fetch('/user/preferences', {
-			method: 'POST',
-			body: JSON.stringify({ region: selectedRegion.value, username })
-		});
-
-		if (resp.ok) {
-			toast.push('Saved changes!', { theme: successTheme });
-			return;
-		}
-
-		toast.push('Something went wrong!', { theme: errorTheme });
-	};
 </script>
 
-<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pb-4">Preferences</h1>
-<div class="flex flex-col gap-4">
-	<div>
-		<Label for="username">Username:</Label>
-		<Input type="text" id="username" bind:value={username} />
-	</div>
-	<div>
-		<Label for="region">Region:</Label>
-		<RegionSelect
-			id="region"
-			{selectedRegion}
-			onSelect={(selection) => {
-				selectedRegion = selection;
-			}}
-		/>
-	</div>
-	<div>
-		<Button on:click={() => handleSave()}>Save changes</Button>
-	</div>
-</div>
+<UserPreferences initialRegion={data.region} initialUsername={data.userName} />
---
+++ src/routes/user/preferences/UserPreferences.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import RegionSelect from '$lib/components/regionSelect/regionSelect.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import { errorTheme, successTheme } from '$lib/toaster';
+	import { toast } from '@zerodevx/svelte-toast';
+
+	export let initialRegion: { value: string; label: string } | undefined;
+	export let initialUsername: string | undefined;
+
+	let selectedRegion = initialRegion ?? { value: '', label: '' };
+	let username = initialUsername ?? '';
+
+	const handleSave = async () => {
+		const resp = await fetch('/user/preferences', {
+			method: 'POST',
+			body: JSON.stringify({ region: selectedRegion.value, username })
+		});
+
+		if (resp.ok) {
+			toast.push('Saved changes!', { theme: successTheme });
+			return;
+		}
+
+		toast.push('Something went wrong!', { theme: errorTheme });
+	};
+</script>
+
+<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pb-4">Preferences</h1>
+<div class="flex flex-col gap-4">
+	<div>
+		<Label for="username">Username:</Label>
+		<Input type="text" id="username" bind:value={username} />
+	</div>
+	<div>
+		<Label for="region">Region:</Label>
+		<RegionSelect
+			id="region"
+			{selectedRegion}
+			onSelect={(selection) => {
+				selectedRegion = selection;
+			}}
+		/>
+	</div>
+	<div>
+		<Button on:click={() => handleSave()}>Save changes</Button>
+	</div>
+</div>
