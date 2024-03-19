1e0c45196e80cde43af30b72b8055380df740092
Brayden Di Luca <bray.diluca@estimateone.com>
2024-03-20 10:09:39 +1100

    Update drizzle schema to use Turso and SQLite

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { Config } from 'drizzle-kit';
 
-const { DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST } = process.env;
+const { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } = process.env;
 
 export default {
 	schema: './src/lib/db/schema.ts',
 	out: './src/lib/db/drizzle',
-	driver: 'mysql2',
+	driver: 'turso',
 	dbCredentials: {
-		connectionString: `mysql://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}/cinemusv2?ssl={"rejectUnauthorized":true}`
+		url: TURSO_DATABASE_URL!,
+		authToken: TURSO_AUTH_TOKEN
 	}
 } satisfies Config;
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,13 +1,7 @@
-import { drizzle } from 'drizzle-orm/planetscale-serverless';
-import { connect } from '@planetscale/database';
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
 import * as schema from './schema';
-import { DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD } from '$env/static/private';
+import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from '$env/static/private';
 
-// create the connection
-const connection = connect({
-	host: DATABASE_HOST,
-	username: DATABASE_USERNAME,
-	password: DATABASE_PASSWORD
-});
-
-export const db = drizzle(connection, { schema });
+const client = createClient({ url: TURSO_DATABASE_URL, authToken: TURSO_AUTH_TOKEN });
+export const db = drizzle(client, { schema });
