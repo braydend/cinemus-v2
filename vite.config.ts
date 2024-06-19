38b56546f115dc354ccc7f8acb8d3095c03b37fa
Brayden <braydendiluca@gmail.com>
2024-06-19 18:40:25 +1000

    Add custom manifest data

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,27 @@ import { defineConfig } from 'vitest/config';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				short_name: 'Cinemus',
+				name: 'Cinemus',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				icons: [
+					{
+						src: '/og-image.png',
+						sizes: '1080x1080',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		globals: true,
