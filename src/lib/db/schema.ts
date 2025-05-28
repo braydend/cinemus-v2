d9f289e8db5a18439680c87f87607923a459ba66
braydend <braydendiluca@gmail.com>
2025-05-28 12:01:56 +0000

    Update authjs and schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -114,10 +114,44 @@ export const userPreferencesRelations = relations(userPreferences, ({ one }) => 
 // 	image: text('image', { length: 255 })
 // });
 
+// export const users = sqliteTable("user", {
+//   id: text("id")
+//     .primaryKey()
+//     .$defaultFn(() => crypto.randomUUID()),
+//   name: text("name"),
+//   email: text("email").unique(),
+//   emailVerified: integer("emailVerified", { mode: "timestamp_ms" }),
+//   image: text("image"),
+// })
+ 
+// export const accounts = sqliteTable(
+//   "account",
+//   {
+//     userId: text("userId")
+//       .notNull()
+//       .references(() => users.id, { onDelete: "cascade" }),
+//     type: text("type").$type<AdapterAccountType>().notNull(),
+//     provider: text("provider").notNull(),
+//     providerAccountId: text("providerAccountId").notNull(),
+//     refresh_token: text("refresh_token"),
+//     access_token: text("access_token"),
+//     expires_at: integer("expires_at"),
+//     token_type: text("token_type"),
+//     scope: text("scope"),
+//     id_token: text("id_token"),
+//     session_state: text("session_state"),
+//   },
+//   (account) => ({
+//     compoundKey: primaryKey({
+//       columns: [account.provider, account.providerAccountId],
+//     }),
+//   })
+// )
+
 export const users = sqliteTable('user', {
-	id: text('id').notNull().primaryKey(),
+	id: text('id').primaryKey().$defaultFn(() => crypto.randomUUID()),
 	name: text('name'),
-	email: text('email').notNull(),
+	email: text('email').unique(),
 	emailVerified: integer('emailVerified', { mode: 'timestamp_ms' }),
 	image: text('image')
 });
@@ -148,7 +182,9 @@ export const accounts = sqliteTable(
 		session_state: text('session_state')
 	},
 	(account) => ({
-		compoundKey: primaryKey(account.provider, account.providerAccountId)
+		compoundKey: primaryKey({
+		columns: [account.provider, account.providerAccountId],
+		}),
 	})
 );
 
